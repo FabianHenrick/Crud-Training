d462dbc9162a40de201285d8e9352c07d1df6dd2
FabianHenrick <fabianhenrick@gmail.com>
2024-08-03 22:57:03 -0300

    Structuring Project

---
+++ Crud.js
@@ -0,0 +1,24 @@
+console.log('Olá, Pessoas!');
+console.log('#ConstruindoCruds');
+
+//[CRUD] JavaScripit Básico.
+
+const miniTwitter = {
+    usuarios:[
+        {
+            username:'Hendriko',
+        }
+    ],
+    posts:[
+        {
+            owner:'Hendriko',
+            content:'Meu Primeiro Tweet',
+        }
+    ],
+
+}
+
+//CREATE
+//READ
+//UPDATE
+//DELETE
